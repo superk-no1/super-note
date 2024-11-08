2419d62ba0061c6d4169f5fcc295a36880e1cfb9
kezhichao <kezhichao@lightgogogo.com>
2024-11-08 11:06:51 +0800

    first commit

---
+++ super_note_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:super_note_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
